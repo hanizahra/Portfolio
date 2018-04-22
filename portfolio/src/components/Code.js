a36801a2f21fbc90e4c8507fd89dd7b30593b5b7
General Assembly <generalassembly@Generals-MacBook-Air.local>
2018-04-21 23:55:22 -0400

    almost done with code page

--- portfolio/src/components/Code.js
+++ portfolio/src/components/Code.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AutoFitImage from 'react-image-autofit-frame';
+import { Image } from 'react-bootstrap';
 import '../App.css';
 
 class Code extends React.Component {
@@ -14,7 +15,8 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>A Tinder-esque ios app where users swipe for matches currently located at their address</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/testimage.png" responsive />
+                <Image className='single-image' src="/swiping.gif" responsive />
               </div>
               <p><b>Tech:</b> React Native/Geolocation/Google APIs(Geocode+Place Details)/Node+Express/PostgreSQL</p>
             </div>
@@ -24,7 +26,7 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>A party listing community where people can create profiles and request party invitations</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/house-party-iii.gif" responsive />
               </div>
               <p><b>Tech:</b> Python+Django/Bootstrap 4</p>
             </div>
@@ -34,7 +36,7 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>Generator of highest upvoted knock knock jokes on select Reddit subreddits</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/jokebook2.png" responsive />
               </div>
               <p><b>Tech:</b> Ruby on Rails/Bootstrap 4</p>
             </div>
@@ -44,7 +46,7 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>A pet finder for rescue animals</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/petsy.gif" responsive />
               </div>
               <p><b>Tech:</b> React JS/Node+Express/PostgreSQL/Bootstrap3</p>
             </div>
@@ -54,7 +56,7 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>A violation and complaint lookup for buildings/apartments in New York</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/building-311.gif" responsive />
               </div>
               <p><b>Tech:</b> Web scraper(Cheerio)/Node+Express/EJS/PostgreSQL/Bootstrap 4</p>
             </div>
@@ -64,7 +66,7 @@ class Code extends React.Component {
             <div className='pieces-text'>
               <p>A browser-based game based on the classic 80's arcade game, Ms. PacMan</p>
               <div className='project-images'>
-                <AutoFitImage frameWidth="300px" frameHeight="200px" imgSrc="https://dummyimage.com/300x100/00ffee/fff" />
+                <Image className='single-image' src="/ms-pac-man.gif" responsive />
               </div>
               <p><b>Tech:</b> JavaScript/Illustrator</p>
             </div>
