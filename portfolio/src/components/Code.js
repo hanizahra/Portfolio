b68f05d946ae8812ae8efd3c5bb55e3c339e8c8d
General Assembly <generalassembly@Generals-MacBook-Air.local>
2018-04-22 20:27:54 -0400

    added links to code gifs and fixed bio alignment

--- portfolio/src/components/Code.js
+++ portfolio/src/components/Code.js
@@ -14,8 +14,10 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/Tinderless-Frontend'>Tinderless</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image1' src="/testimage.png" responsive />
-                <Image className='single-image1' src="/swiping.gif" responsive />
+                <a href='https://github.com/hanizahra/Tinderless-Frontend'>
+                  <Image className='single-image1' src="/testimage.png" responsive />
+                  <Image className='single-image1' src="/swiping.gif" responsive />
+                </a>
               </div>
               <p className='descript'>Users swipe for matches currently located at their address</p>
               <p className='tech'><b>Tech:</b> React Native/Geolocation/Google APIs/{<br />}Node+Express/PostgreSQL</p>
@@ -25,7 +27,9 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/House-Party-III'>House Party III</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image' src="/house-party-iii.gif" responsive />
+                <a href='https://github.com/hanizahra/House-Party-III'>
+                  <Image className='single-image' src="/house-party-iii.gif" responsive />
+                </a>
               </div>
               <p className='descript'>Users can list and request invites to parties</p>
               <p className='tech'><b>Tech:</b> Python+Django/Bootstrap 4</p>
@@ -35,7 +39,9 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/Reddit-Joke-Book'>Reddit JokeBook</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image' src="/jokebook2.png" responsive />
+                <a href='https://github.com/hanizahra/Reddit-Joke-Book'>
+                  <Image className='single-image' src="/jokebook2.png" responsive />
+                </a>
               </div>
               <p className='descript'>Random generator of most upvoted knock knock jokes</p>
               <p className='tech'><b>Tech:</b> Ruby on Rails/Bootstrap 4</p>
@@ -45,7 +51,9 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/Petsy'>Petsy</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image' src="/petsy.gif" responsive />
+                <a href='https://github.com/hanizahra/Petsy'>
+                  <Image className='single-image' src="/petsy.gif" responsive />
+                </a>
               </div>
               <p className='descript'>A pet finder for rescue animals</p>
               <p className='tech'><b>Tech:</b> React JS/Node+Express/PostgreSQL/Bootstrap3</p>
@@ -55,7 +63,9 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/Building-311'>Building 311</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image' src="/building-311.gif" responsive />
+                <a href='https://github.com/hanizahra/Building-311'>
+                  <Image className='single-image' src="/building-311.gif" responsive />
+                </a>
               </div>
               <p className='descript'>Violations and Complaints for {<br />}buildings/apartments in New York</p>
               <p className='tech'><b>Tech:</b> Web scraper(Cheerio)/Node+Express/{<br />}EJS/PostgreSQL/Bootstrap 4</p>
@@ -65,7 +75,9 @@ class Code extends React.Component {
             <h2><a href='https://github.com/hanizahra/Ms-Pacman'>Ms. PacMan</a></h2>
             <div className='pieces-text'>
               <div className='project-images'>
-                <Image className='single-image' src="/ms-pac-man.gif" responsive />
+                <a href='https://github.com/hanizahra/Ms-Pacman'>
+                  <Image className='single-image' src="/ms-pac-man.gif" responsive />
+                </a>
               </div>
               <p className='descript'>A browser-based 80's arcade game, Ms. PacMan</p>
               <p className='tech'><b>Tech:</b> JavaScript/Illustrator</p>
