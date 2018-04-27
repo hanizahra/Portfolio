6a140170b333af51c8d65a749ee037a1872e07e1
Hani Zahra <hanizahra@Hanis-MacBook-Pro-7.local>
2018-04-27 13:23:47 -0400

    fixed sidebar in about me

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -5,6 +5,7 @@ class About extends React.Component {
   render(){
     return(
       <div>
+        <p>&nbsp;</p>
         <h1 className='page-title1'>All About Me</h1>
         <div className='page-container'>
           <div classname='body-text'>
